448daf20f5a1fcfb9fcab1472851f4cb55606765
caicai <940002581@qq.com>
2026-09-21 19:28:02 +0800

    维护：更新用户脚本元数据

--- Bilibli-Reader.user.js
+++ Bilibli-Reader.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
-// @name         Bilibili Reader｜B站阅读模式
+// @name         Bilibili Reader｜哔哩哔哩阅读模式
 // @namespace    https://github.com/bilibli-reader
-// @version      0.0.3
+// @version      0.0.4
 // @description  将 B 站视频切换为视频、章节与字幕联动的阅读视图
 // @author       Local
+// @license      MIT
 // @homepageURL  https://github.com/Wow-CaiCai/Bilibli-Reader
 // @source       https://github.com/Wow-CaiCai/Bilibli-Reader
 // @supportURL   https://github.com/Wow-CaiCai/Bilibli-Reader/issues
--- release/bilibli-reader-v0.0.3.user.js
+++ release/bilibli-reader-v0.0.3.user.js
@@ -4,6 +4,7 @@
 // @version      0.0.3
 // @description  将 B 站视频切换为视频、章节与字幕联动的阅读视图
 // @author       Local
+// @license      MIT
 // @homepageURL  https://github.com/Wow-CaiCai/Bilibli-Reader
 // @source       https://github.com/Wow-CaiCai/Bilibli-Reader
 // @supportURL   https://github.com/Wow-CaiCai/Bilibli-Reader/issues
