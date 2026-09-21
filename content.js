08d8a4cfd7c05cee8a5dc810bf8e6709269b0cf2
caicai <940002581@qq.com>
2026-09-21 19:28:02 +0800

    功能：完善合集功能

--- content.js
+++ content.js
@@ -46,7 +46,7 @@ const DEFAULT_SETTINGS = {
 };
 const PLAYER_AI_ICON_VARIANT = "badge";
 
-const READER_VERSION = "0.0.1";
+const READER_VERSION = "0.0.3";
 const CACHE_KEY_PREFIX = "bilibli_reader_subtitle_cache_";
 globalThis.__BILIBLI_READER_CONTENT_SCRIPT_LOADED__ = READER_VERSION;
 const state = {
